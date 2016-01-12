49d724d00e89f3ecfd98f9250a3f5b0a9ab97a18
Magicolo <straty91@hotmail.com>
2016-01-11 20:32:54 -0500

    Updated Pseudo

--- Assets/NRefactoryTest/zTest.cs
+++ Assets/NRefactoryTest/zTest.cs
@@ -11,15 +11,77 @@ using Pseudo.Internal.Audio;
 using System.Runtime.Serialization;
 using Pseudo.Internal;
 using UnityEngine.SceneManagement;
+using Pseudo.Internal.Entity3;
 
 public class zTest : PMonoBehaviour
 {
-	const int iterations = 1000;
+	SystemManager systemManager;
 
 	[Button]
 	public bool test;
 	void Test()
 	{
-		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex == 0 ? 1 : 0);
 	}
+
+	void Awake()
+	{
+		systemManager = new SystemManager();
+		//systemManager.AddSystem(new MotionSystem(systemManager));
+	}
+
+	void Update()
+	{
+		systemManager.Update();
+	}
+
+	void FixedUpdate()
+	{
+		systemManager.FixedUpdate();
+	}
+
+	void LateUpdate()
+	{
+		systemManager.LateUpdate();
+	}
+}
+
+//public class MotionSystem : SystemBase, IFixedUpdateable
+//{
+//	public bool Active { get; set; }
+
+//Pseudo.Internal.Entity3.IEntityGroup movables = EntityManager.GetEntityGroup(new Type[] { typeof(IMotionComponent), typeof(IPositionComponent) });
+
+//public MotionSystem(ISystemManager systemManager) : base(systemManager) { }
+
+//public void FixedUpdate()
+//{
+//	for (int i = 0; i < movables.Entities.Count; i++)
+//	{
+//		var movable = movables.Entities[i];
+//		var position = movable.GetComponent<IPositionComponent>();
+//		var motion = movable.GetComponent<IMotionComponent>();
+
+//		position.Position += Vector3.right * motion.Speed;
+//	}
+//}
+//}
+
+public class MotionComponent : IMotionComponent
+{
+	public float Speed { get; set; }
+}
+
+public interface IMotionComponent : Pseudo.Internal.Entity3.IComponent
+{
+	float Speed { get; set; }
+}
+
+public class PositionComponent : IPositionComponent
+{
+	public Vector3 Position { get; set; }
+}
+
+public interface IPositionComponent : Pseudo.Internal.Entity3.IComponent
+{
+	Vector3 Position { get; set; }
 }
